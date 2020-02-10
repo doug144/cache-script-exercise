4d62915ab1f60cad0e9553d63feb90ef57490561
Douglas Gross <doug144@gmail.com>
2020-02-10 16:28:28 +0200

    Add project files.

---
+++ ClassLibrary/HashCodeCalulator.cs
@@ -0,0 +1,18 @@
+﻿using PubComp.Caching.AopCaching;
+
+namespace ClassLibrary
+{
+    public class HashCodeCalulator
+    {
+        [Cache("MyCacheName")]
+        public int GetHashCode(string key)
+        {
+            return Get(key);
+        }
+
+        private int Get(string key)
+        {
+            return key.GetHashCode();
+        }
+    }
+}
---
+++ ClassLibrary2/HashCodeCalulator2.cs
@@ -0,0 +1,18 @@
+﻿using PubComp.Caching.AopCaching;
+
+namespace ClassLibrary2
+{
+    public class HashCodeCalulator2
+    {
+        [Cache("MyOtherCacheName")]
+        public int GetHashCode(string key)
+        {
+            return Get(key);
+        }
+
+        private int Get(string key)
+        {
+            return key.GetHashCode();
+        }
+    }
+}
